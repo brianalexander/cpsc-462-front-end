ca7e4a365e353e261a8d3ce4edbc9e7add2fbe76
Brian Alexander <balexander28@csu.fullerton.edu>
2020-02-22 14:08:04 -0800

    add skeleton for chat window

---
+++ app/src/components/chat/ChatInputBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ChatInputBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ChatInputBox;
