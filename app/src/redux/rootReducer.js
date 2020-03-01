afb87693fe507e5f21946112940e8c2b28b54c2c
Brian Alexander <balexander28@csu.fullerton.edu>
2020-03-01 12:35:35 -0800

    add lobby to store

--- app/src/redux/rootReducer.js
+++ app/src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userListReducer from "./userListSlice";
 import chatReducer from "./chatSlice";
+import lobbyReducer from "./lobbySlice";
 
 const rootReducer = combineReducers({
   userList: userListReducer,
-  chat: chatReducer
+  chat: chatReducer,
+  lobby: lobbyReducer
 });
 
 export default rootReducer;
